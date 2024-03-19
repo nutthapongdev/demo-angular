667d1bc275b5b0b596ced92336ad29d7605878cd
nutthapong piboonwittayagul <nattapong.piboonwittayagul@krungsrinimble.com>
2024-03-19 13:50:05 +0700

    working => storage

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import { UserForm } from '../models/user-form';
 import { AuthService } from '../services/auth.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,7 @@ import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
 export class RegisterComponent {
   form: UserForm = new UserForm();
 
-  constructor(private authService: AuthService, private dialog: MatDialog){
+  constructor(private authService: AuthService, private dialog: MatDialog, private storeageService: StorageService){
     
   }
   
@@ -22,6 +23,8 @@ export class RegisterComponent {
     console.log('Username:', username);
     console.log('Email:', email);
     console.log('Password:', password);
+
+    this.storeageService.saveUser(this.form)
 
     this.authService.register(username, email, password).subscribe({
       next: data => {
